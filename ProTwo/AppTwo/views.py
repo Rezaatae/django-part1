5ce8a81579363eda37868329a9030466d28b2890
rezaatae <r.atae@yahoo.com>
2021-11-08 21:07:40 +0000

    challenge

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+
+def help(request):
+    helpdict = {'help_insert':'HELP PAGE!'}
+    return render(request,'AppTwo/help.html',context=helpdict)
